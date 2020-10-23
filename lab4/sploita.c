df4ec227ce41a50f717e54294a96209b4b299c07
ishanphadke <44324805+ishanphadke@users.noreply.github.com>
2020-10-23 15:45:35 -0500

    Testing a

--- lab4/sploita.c
+++ lab4/sploita.c
@@ -7,7 +7,9 @@
 
 int main(void)
 {
-  writecmd(PIPEPATH, "Hello from sploitasdddddddddddddddddddddddddddddddddddddddddddddd!");
+  writecmd(PIPEPATH, "%08x!");
   
   return 0;
 }
+// esp: 0xbfbf6710
+// buf: 0xbfbf671d
